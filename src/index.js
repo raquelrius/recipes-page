5587e639f4cc0efacfd30e99eedd95ce05a63e34
raquelrius <raquelrius@hotmail.com>
2021-03-07 10:29:27 +0100

    add recipe base structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-/**/
+const imagesUrl = ['../src/katsukare.jpeg', '../src/omuraisu.jpg'];
+
+imagesUrl.forEach(imageUrl => {
+  const recipe = document.createElement('div');
+  recipe.classList.add('recipe');
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('imageContainer');
+  const image = document.createElement('img');
+  image.src = imageUrl;
+  imageContainer.appendChild(image);
+  recipe.appendChild(imageContainer);
+
+  document.body.appendChild(recipe);
+})
