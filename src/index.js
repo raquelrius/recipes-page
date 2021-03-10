fe99f7073896225cfe4b8974e14ffd332ac530ec
raquelrius <raquelrius@hotmail.com>
2021-03-10 20:08:23 +0100

    added style and images

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-const imagesUrl = ['../src/katsukare.jpeg', '../src/omuraisu.jpg'];
+const imagesUrl = ['../src/katsukare.jpg', '../src/salmonteriyaki.jpg', '../src/omuraisu.jpg'];
 
 imagesUrl.forEach(imageUrl => {
-  const recipe = document.createElement('div');
-  recipe.classList.add('recipe');
   const imageContainer = document.createElement('div');
   imageContainer.classList.add('imageContainer');
   const image = document.createElement('img');
   image.src = imageUrl;
   imageContainer.appendChild(image);
-  recipe.appendChild(imageContainer);
-
-  document.querySelector('main').appendChild(recipe);
-})
+  document.getElementById('recipe-photo-list').appendChild(imageContainer);
+});
